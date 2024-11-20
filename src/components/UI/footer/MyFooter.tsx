fcf195f09d3d5607d47faf16694610e36b232f6d
freefalltu <freefalltu@gmail.com>
2024-11-21 02:50:50 +0300

    созданы переиспользуемые компоненты header и footer

---
+++ src/components/UI/footer/MyFooter.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import classes from './MyFooter.module.scss'
+
+const MyFooter = () => {
+    return (
+        <footer className={classes.footer}>
+            <div className={classes.footer__container}>
+                <Link className={classes.container__logo} to='/'>Goods4you</Link>
+                <nav>
+                    <Link className={classes.nav__btn} to='/'>Catalog</Link>
+                    <Link className={classes.nav__btn} to='/'>FAQ</Link>
+                </nav>
+            </div>
+        </footer>
+    )
+}
+
+export default MyFooter
---
+++ src/components/UI/header/MyHeader.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import classes from './MyHeader.module.scss'
+
+const MyHeader = () => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.header__container}>
+                <Link className={classes.container__logo} to='/'>Goods4you</Link>
+                <nav>
+                    <Link className={classes.nav__btn} to='/'>Catalog</Link>
+                    <Link className={classes.nav__btn} to='/'>FAQ</Link>
+                    <Link className={classes.nav__btn} to='/'>Cart</Link>
+                    <Link className={classes.nav__btn} to='#'>Johnson Smith</Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default MyHeader
