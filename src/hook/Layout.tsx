f32163d2cb59658940207e5f7496c5dce553d8a9
freefalltu <freefalltu@gmail.com>
2024-11-23 23:41:26 +0300

    add navbar

--- src/hook/Layout.tsx
+++ src/hook/Layout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../components/footer/Footer"
-import Header from "../components/header/Header"
+import NavBar from "../components/navbar/NavBar"
 
 
 const Layout = () => {
     return (
         <div id="Main" className="wrapper">
             <div className="wrapper__header">
-                <Header />
+                <NavBar />
             </div>
             <main className="wrapper__container">
                 <Outlet />
