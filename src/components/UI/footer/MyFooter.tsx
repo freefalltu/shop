f125c6a00b8045c93d157f5121e99cc9a0a96837
freefalltu <freefalltu@gmail.com>
2024-11-23 09:47:57 +0300

    скорректирован роутинг и стили

--- src/components/UI/footer/MyFooter.tsx
+++ src/components/UI/footer/MyFooter.tsx
@@ -7,8 +7,8 @@ const MyFooter = () => {
             <div className={classes.footer__container}>
                 <Link className={classes.container__logo} to='/'>Goods4you</Link>
                 <nav>
-                    <Link className={classes.nav__btn} to='/'>Catalog</Link>
-                    <Link className={classes.nav__btn} to='/'>FAQ</Link>
+                    <Link className={classes.nav__btn} to='/#catalog'>Catalog</Link>
+                    <Link className={classes.nav__btn} to='/#faq'>FAQ</Link>
                 </nav>
             </div>
         </footer>
