c917577e20d078290cb244f671b4896461b778b3
freefalltu <freefalltu@gmail.com>
2024-11-28 16:03:48 +0300

    Верстка cart

---
+++ src/components/cart/Cart.tsx
@@ -0,0 +1,59 @@
+import cl from './Cart.module.scss'
+import photo from '../../img/cart/photo_cart.svg'
+import Counter from '../UI/counter/Counter'
+
+const Cart = () => {
+    return (
+        <div className={cl.cart}>
+            <h1 className={cl.cart__title}>My cart</h1>
+            <div className={cl.cart__wrapper}>
+                <div className={cl.products}>
+                    <div className={cl.product}>
+                        <img src={photo} alt="" />
+                        <div className={cl.product__container}>
+                            <p className={cl.container__title}>Essence Mascara Lash Princess</p>
+                            <p className={cl.container__price}>$110</p>
+                        </div>
+                        <Counter size='large' />
+                        <p className={cl.product__delete}>Delete</p>
+                    </div>
+                    <div className={cl.product}>
+                        <img src={photo} alt="" />
+                        <div className={cl.product__container}>
+                            <p className={cl.container__title}>Essence Mascara Lash Princess</p>
+                            <p className={cl.container__price}>$110</p>
+                        </div>
+                        <Counter size='large' />
+                        <p className={cl.product__delete}>Delete</p>
+                    </div>
+                    <div className={cl.product}>
+                        <img src={photo} alt="" />
+                        <div className={cl.product__container}>
+                            <p className={cl.container__title}>Essence Mascara Lash Princess</p>
+                            <p className={cl.container__price}>$110</p>
+                        </div>
+                        <Counter size='large' />
+                        <p className={cl.product__delete}>Delete</p>
+                    </div>
+                </div>
+                <div className={cl.price}>
+                        <div className={cl.total__count}>
+                            <p className={cl.count__text}>Total count</p>
+                            <p className={cl.count__cost}>3 items</p>
+                        </div>
+                        <div className={cl.discount}>
+                            <p className={cl.discount__text}>Price without discount</p>
+                            <p className={cl.discount__cost}>$700</p>
+                        </div>
+                        <hr className={cl.price__line}/>
+                        <div className={cl.total__price}>
+                            <p className={cl.price__text}>Total price</p>
+                            <p className={cl.price__cost}>$590</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cart
--- src/pages/Cartpage.tsx
+++ src/pages/Cartpage.tsx
@@ -1,9 +1,10 @@
 import useTitle from "../hook/useTitle"
+import Cart from '../components/cart/Cart'
 
 const Cartpage = () => {
     useTitle('My cart | Goods4you')
     return (
-        <div>cart</div>
+        <Cart/>
     )
 }
 
