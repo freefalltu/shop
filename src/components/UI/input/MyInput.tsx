697c7bb342b98390e797949ca66089bb364381ba
freefalltu <freefalltu@gmail.com>
2024-11-30 01:54:05 +0300

    удалены приставки My

--- src/components/UI/input/MyInput.tsx
+++
@@ -1,9 +0,0 @@
-import cl from './MyInput.module.scss'
-
-const MyInput = () => {
-    return (
-        <input type='text' className={cl.input} placeholder='Search by title' />
-    )
-}
-
-export default MyInput
