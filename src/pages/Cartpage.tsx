8e7014c2cca31bcc9efa1f06701a72a173c59db9
freefalltu <freefalltu@gmail.com>
2024-12-04 18:18:09 +0300

    delete pages

--- src/pages/Cartpage.tsx
+++
@@ -1,7 +0,0 @@
-import { Cart } from "src/components/cart";
-import useTitle from "../hook/useTitle";
-
-export const Cartpage = () => {
-  useTitle("My cart | Goods4you");
-  return <Cart />;
-};
--- src/pages/Productpage.tsx
+++
@@ -1,7 +0,0 @@
-import { Product } from "src/components/product";
-import useTitle from "../hook/useTitle";
-
-export const Productpage = () => {
-  useTitle("Essence Mascara Lash Princess | Goods4you");
-  return <Product />;
-};
