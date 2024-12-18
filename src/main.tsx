de2cfc135f745a28ffc45cd289bfe42fad21f833
freefalltu <freefalltu@gmail.com>
2024-12-18 12:58:06 +0300

    add provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { setupStore } from "./store/store.ts";
+
+const store = setupStore();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
 );
