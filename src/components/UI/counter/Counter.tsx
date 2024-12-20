e9a2ffc3d0b2893615c74885d413fa462cfda87f
freefalltu <freefalltu@gmail.com>
2024-12-20 19:37:38 +0300

    правки в counter

--- src/components/UI/counter/Counter.tsx
+++ src/components/UI/counter/Counter.tsx
@@ -1,38 +1,37 @@
 import cn from "classnames";
 import cl from "./Counter.module.scss";
-import React, { MouseEventHandler, useState } from "react";
+import React, { MouseEventHandler } from "react";
 import { Button } from "../button";
 
 interface CounterProps {
   size?: "medium" | "large";
   children: number;
   onClick?: MouseEventHandler<HTMLDivElement>;
+  onMinusClick?: MouseEventHandler<HTMLButtonElement>;
+  onPlusClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export const Counter: React.FC<CounterProps> = ({
   size = "medium",
   children,
   onClick,
+  onMinusClick,
+  onPlusClick,
 }) => {
-  const [count, setCount] = useState(children);
   return (
     <div className={cl.counter} onClick={onClick}>
       <Button
         className={cn(cl.myBtn, cl[size])}
         view="icon"
-        onClick={() => {
-          setCount((value) => value - 1);
-        }}
+        onClick={onMinusClick}
       >
         <div className={cl.minus} />
       </Button>
-      <span>{count} items</span>
+      <span>{children} items</span>
       <Button
         className={cn(cl.myBtn, cl[size])}
         view="icon"
-        onClick={() => {
-          setCount((value) => value + 1);
-        }}
+        onClick={onPlusClick}
       >
         <div className={cl.plus} />
       </Button>
