613cb024a1e812c5ee711609e175b714ba332dfc
freefalltu <freefalltu@gmail.com>
2024-12-20 19:31:21 +0300

    delete components

--- src/api/info-products-api.ts
+++
@@ -1,6 +0,0 @@
-import axiosBase from "./axiosBase";
-
-export const fetchProductInfo = async (id: number) => {
-  const response = await axiosBase.get(`/products/${id}`, {});
-  return response.data;
-};
