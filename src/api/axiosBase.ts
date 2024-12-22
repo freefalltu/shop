36a9a102893793712cda16c4beef31b1b12ae7f1
freefalltu <freefalltu@gmail.com>
2024-12-22 19:04:22 +0300

    адрес вынесен в переменную

--- src/api/axiosBase.ts
+++ src/api/axiosBase.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseUrl = "https://dummyjson.com";
+
 const axiosBase = axios.create({
-  baseURL: "https://dummyjson.com",
+  baseURL: baseUrl,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
