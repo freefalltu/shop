26c7bc2f897ac2471d41ceb7c0a78e94f9f754a2
freefalltu <freefalltu@gmail.com>
2024-12-22 18:47:01 +0300

    отредактирована страница notfound

--- src/pages/NotfoundPage/NotfoundPage.tsx
+++ src/pages/NotfoundPage/NotfoundPage.tsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button } from "src/components/UI/button";
+import { Title } from "src/components/UI/title";
 import useTitle from "src/hook/useTitle";
+import cl from "./NotfoundPage.module.scss";
 
 export const NotfoundPage = () => {
   useTitle("Not found page");
-  return <div>not found</div>;
+  return (
+    <div className={cl.wrapper}>
+      <Title tag="h1" fontSize="xxl" fontWeight="Bold" className={cl.title}>
+        404 Page not found
+      </Title>
+      <Link to="/">
+        <Button view="text" size="big" className={cl.myBtn}>
+          Return to main page
+        </Button>
+      </Link>
+    </div>
+  );
 };
