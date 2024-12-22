0bf65a23a5b5f096599e4f9cf5d00c5bcff3dba6
freefalltu <freefalltu@gmail.com>
2024-12-22 18:42:07 +0300

    удалены лишние компоненты

--- src/utils/removeEmptyParams.ts
+++
@@ -1,9 +0,0 @@
-export const removeEmptyParams = (
-  params: Record<string | number, string | number>,
-) => {
-  for (const key in params) {
-    if (params[key] === "" || params[key] === null) {
-      delete params[key as keyof typeof params];
-    }
-  }
-};
