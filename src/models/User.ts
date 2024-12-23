d98c74a24e624c9bc7809aa615448ff46ca305fb
freefalltu <freefalltu@gmail.com>
2024-12-24 01:17:46 +0300

    удалён компонент

--- src/models/User.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from "./Product";
-
-export interface User {
-  totalQuantity: number;
-  totalProducts: number;
-  discountedTotal: number;
-  total: number;
-  products: Product[];
-}
