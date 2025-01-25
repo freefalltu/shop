6cf34b2cef5c83cc2c72ce05fd96ac7e63d52614
freefalltu <freefalltu@gmail.com>
2025-01-25 10:25:52 +0300

    правки в LoginPage

--- src/UI/input/Input.tsx
+++ src/UI/input/Input.tsx
@@ -6,19 +6,24 @@ interface InputProps {
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   className?: string;
+  value?: string;
+  type?: string;
 }
 
 export const Input: React.FC<InputProps> = ({
   onChange,
   placeholder = "Search by title",
   className,
+  value,
+  type,
 }) => {
   return (
     <input
-      type="text"
+      type={type}
       className={cn(className, cl.input)}
       placeholder={placeholder}
       onChange={onChange}
+      value={value}
     />
   );
 };
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,47 @@
-import { Authorization } from "components/authorization";
+import cl from "./LoginPage.module.scss";
 import { LoginHeader } from "components/loginHeader";
 import useTitle from "hook/useTitle";
+import { useState } from "react";
+import { Button } from "UI/button";
+import { Input } from "UI/input";
+import { Title } from "UI/title";
 
 export const LoginPage = () => {
   useTitle("Sign in | Goods4you");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
     <div>
       <LoginHeader />
-      <Authorization />
+      <div className={cl.container}>
+        <Title tag="h1" fontWeight="Bold" fontSize="xxl" className={cl.title}>
+          Sign in
+        </Title>
+        <div className={cl.box}>
+          <Input
+            className={cl.input}
+            placeholder="Login"
+            value={email}
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            className={cl.input}
+            placeholder="Password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="myBtnText"
+            view="text"
+            size="small"
+            className={cl.button}
+          >
+            Sign in
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
